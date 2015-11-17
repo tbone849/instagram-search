faafbc4b37223141174bce3423a519b4945a2b34
Tim Sarvey <timsarvey@gmail.com>
2015-11-17 15:57:04 -0500

    Now brings in images from instagram.

--- js/app.js
+++ js/app.js
@@ -3,24 +3,28 @@ angular.module('instagramSearchApp', ['ngMessages', 'ngAnimate'])
 		$scope.tag = '';
 		$scope.results = [];
 		$scope.submitError = false;
+		var timeout = $timeout;
 
 		function showError(){
 			$scope.submitError = true;
-			$timout(function(){
-				console.log($timeout);
+			timeout(function(){
 				$scope.submitError = false;
 			}, 3000);
 		};
 
 		$scope.submit = function(){
-			$http({
-				url: 'https://api.instagram.com/v1/tags/' + $scope.tag + '/media/recent',
+			var params = {
 				callback: 'JSON_CALLBACK',
 				client_id: '80acbb5ef24f4d95a5b50c1c2b987377',
-				method: 'JSONP'
+			}
+			$http({
+				url: 'https://api.instagram.com/v1/tags/' + $scope.tag + '/media/recent',
+				method: 'JSONP',
+				params: params
 			})
 			.then(function(results){
-				$scope.results = results.data;
+				$scope.results = results.data.data;
+				console.log(results.data.data);
 			},
 			function(){
 				showError();
