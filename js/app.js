67765095a39b0186dd1e7e4026dd2ea9e6bb9bf8
Tim Sarvey <timsarvey@gmail.com>
2015-11-18 13:49:23 -0500

    Add animation and error message.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
 angular.module('instagramSearchApp', ['ngMessages', 'ngAnimate'])
 	.controller('searchFormController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout){
 		$scope.tag = '';
+		$scope.tagInput = '';
 		$scope.results = [];
-		$scope.submitError = false;
+		$scope.foundError = false;
+		$scope.foundResults = false;
 		var timeout = $timeout;
 
 		function showError(){
-			$scope.submitError = true;
+			$scope.foundError = true;
 			timeout(function(){
-				$scope.submitError = false;
-			}, 3000);
+				$scope.foundError = false;
+			}, 5000);
 		};
 
 		$scope.submit = function(){
+			$scope.foundResults = false;
+			$scope.tag = $scope.tagInput.split(' ').join('');
+			console.log($scope.tag);
 			var params = {
 				callback: 'JSON_CALLBACK',
 				client_id: '80acbb5ef24f4d95a5b50c1c2b987377',
+				access_token: '2281757929.80acbb5.4986eadf824a4cb1b79774bc475740ec',
+				scope: 'public_content'
 			}
 			$http({
 				url: 'https://api.instagram.com/v1/tags/' + $scope.tag + '/media/recent',
@@ -24,7 +31,9 @@ angular.module('instagramSearchApp', ['ngMessages', 'ngAnimate'])
 			})
 			.then(function(results){
 				$scope.results = results.data.data;
-				console.log(results.data.data);
+				$scope.foundResults = true;
+				$scope.tagInput = '';
+				console.log(results);
 			},
 			function(){
 				showError();
